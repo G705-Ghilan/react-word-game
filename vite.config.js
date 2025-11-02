76dac1833015b886f3ece66b6e777628539f7ae0
G705-Ghilan <G705Github@gmail.com>
2025-11-02 14:54:50 +0300

    update

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+
+// Github pages deploy Guide: https://www.vd-developer.online/blog/vite-react-deploy-github
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
